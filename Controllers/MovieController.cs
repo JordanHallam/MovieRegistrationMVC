46f9f9e6a05dcb151482a9833179214121f5de40
JordanHallam <jghals12@gmail.com>
2020-08-21 16:38:44 -0400

    Initial Commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+
+namespace MovieRegistration.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View(new Movie());
+        }
+
+        public IActionResult RegisterMovie(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Summary", movie);
+            }
+            else
+            {
+                return View("Index", movie);
+            }
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        [Required]
+        [RegularExpression(@"[0-9]{4}",ErrorMessage ="Please enter 4 digit ID")]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =1, ErrorMessage ="Movie title must be 1-50 characters")]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =1, ErrorMessage ="Genre must be 1-50 characrers")]
+        public string Genre { get; set; }
+        
+        //[Range(1840, maximum: double.Parse(DateTime.Today.Year))]
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+    }
+}
